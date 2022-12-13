b16594e6748583d076438dd38dcbd358e6a43c92
zarno-cartercenter <Zack.Arno@cartercenter.org>
2022-12-13 15:57:17 -0500

    integrate `dplyr::recode()` into `clean_vec_names()` - looks safer and more understandable

--- R/harmonizing_admins.R
+++ R/harmonizing_admins.R
@@ -51,30 +51,53 @@ load_admins_from_raw <- function(df){
 
 }
 
+#'
+#'
+#' #' Title
+#' #'
+#' #' @param vec
+#' #'
+#' #' @return
+#' #' @description taken from `janitor::make_clean_names()`
+#' #' @export
+#' #'
+#' #' @examples
+#' clean_vec_names <-  function(vec){
+#'   good_start <- stringr::str_replace(string = {{vec}},
+#'                                      pattern = "\\A[\\h\\s\\p{Punctuation}\\p{Symbol}\\p{Separator}\\p{Other}]*(.*)$",
+#'                                      replacement = "\\1")
+#'   cleaned_within <- stringr::str_replace(string = {{vec}},
+#'                                          pattern = "[\\h\\s\\p{Punctuation}\\p{Symbol}\\p{Separator}\\p{Other}]+",
+#'                                          replacement = ".")
+#'
+#'   cased_names <- snakecase::to_snake_case(cleaned_within)
+#'   return(cased_names)
+#' }
 
 
-#' Title
+#' clean_str_vec
 #'
-#' @param vec
+#' @param x vec
 #'
-#' @return
-#' @description taken from `janitor::make_clean_names()`
+#' @return  vector cleaned with `janitor::make_clean_names()` without unique criteria
 #' @export
 #'
 #' @examples
-clean_vec_names <-  function(vec){
-  good_start <- stringr::str_replace(string = {{vec}},
-                                     pattern = "\\A[\\h\\s\\p{Punctuation}\\p{Symbol}\\p{Separator}\\p{Other}]*(.*)$",
-                                     replacement = "\\1")
-  cleaned_within <- stringr::str_replace(string = {{vec}},
-                                         pattern = "[\\h\\s\\p{Punctuation}\\p{Symbol}\\p{Separator}\\p{Other}]+",
-                                         replacement = ".")
+clean_vec_names<- function(x){
 
-  cased_names <- snakecase::to_snake_case(cleaned_within)
-  return(cased_names)
+  x_unique <- x[!is.na(x)] |>
+    unique()
+
+  x_unique_clean <- x_unique |>
+    janitor::make_clean_names()
+
+  x_lookup <- x_unique_clean |>
+    rlang::set_names(x_unique)
+
+  dplyr::recode(x,!!!x_lookup)
+
+
 }
-
-
 
 
 #' Title
@@ -131,9 +154,18 @@ check_admin2<- function(df, df_master){
   df_master_adm_clean <-  df_master |>
     sanitize_admins() |>
     mutate(adm_1_2 = paste0(adm1_en,"_",adm2_en))
-  problem_freq <- df_adm_san |>
-    filter(!adm_1_2 %in% df_master_adm_clean$adm_1_2) |>
-    count(adm_1_2)
+  problem_records <- df_adm_san |>
+    filter(!adm_1_2 %in% df_master_adm_clean$adm_1_2)
+  if("file_name" %in% colnames(df)){
+    problem_freq <- problem_records |>
+      count(file_name,adm_1_2)
+  }else{
+    problem_freq <- problem_records |>
+      count(adm_1_2)
+  }
+
+
+
 
   if(nrow(problem_freq)==0){
     cat(crayon::green("All admin 2 in raw data are in master list"))
