065b79fb969f3481ca43a8e0ef73bab6ec51ab84
geocrunchy <geocrunchy@gmail.com>
2023-02-20 01:05:57 +0600

    initial_exploring

---
+++ R/tidy_new_data.R
@@ -0,0 +1,18 @@
+rm(list = ls())
+
+library(tidyverse)
+library(readxl)
+library(unheadr)
+source("function_tidy_data.R")
+
+## !!!need to check, env variable not working
+CC_RB_LF_SCH_MONTHLY="/Users/mehedi/Library/CloudStorage/GoogleDrive-geocrunchy@gmail.com/My Drive/carter_center/RBLF/Country Monthly Reports"
+path <-  paste0(CC_RB_LF_SCH_MONTHLY,"/ETH/new_format_example/202203/202203_r1_dawuro_konta_20230218.xlsx")
+
+
+### reading the dataset
+df_list <- read_data(data_path = path,sheet_contains = "Total",
+               cols_words_replace_to_na = c("x_","_for_oncho_lf"))
+
+## check if the column name accross the dataframes are matching
+check_cols <- df_list %>% check_cols_name()
---
+++ function_tidy_data.R
@@ -0,0 +1,51 @@
+
+### read data ##################
+read_data <- function(data_path,
+                      sheet_contains,
+
+                      additional_sheets =NULL,
+                      cols_words_replace_to_na = "x_|_for_oncho_lf"){
+
+  cols_replace_to_na <- paste0(cols_words_replace_to_na,collapse = "|")
+
+  sheet_name <- excel_sheets(data_path)
+  sheet_name_total<- sheet_name[grepl(sheet_contains,x = sheet_name)]
+  sheets_name <- c(additional_sheets,sheet_name_total)
+
+  df_all <- list()
+  for (i in sheets_name) {
+    data_read <- read.xlsx(data_path,sheet = i,skipEmptyRows = T,
+                           skipEmptyCols = T,
+                           startRow = 2,
+                           fillMergedCells = T)  %>%
+      type.convert()  %>% mash_colnames(n_name_rows = 3,sep = ".") %>%
+      mutate(filename = paste0(data_path),
+             tab_name = i)
+    names(data_read) <- names(data_read) %>% snakecase::to_snake_case() %>%
+      str_replace_all(cols_replace_to_na,"")
+    names(data_read) <- gsub("^[0-9]+[//_]","",names(data_read))
+    df_all[[snakecase::to_snake_case(i)]]  <- data_read %>%
+      filter(!is.na(name_of_woredas)) %>%
+      filter(!grepl("total|Total",name_of_woredas))
+  }
+  return(df_all)
+  }
+################ check cols name ###############
+
+check_cols_name <- function(df_list) {
+  cols_name <- map(df_list,~names(.x))
+  all_cols_name <- unlist(cols_name) %>% unique()
+  df <- data.frame(cols_name_all = all_cols_name)
+
+  for (i in names(df_list)) {
+    list_name <- paste0(i)
+    df <- df %>% mutate(!!sym(list_name) := cols_name_all %in% names(df_list[[i]]))
+  }
+
+
+  df %>% filter(!rowSums(df[names(df_list)]) == length(names(df_list)))
+
+}
+
+
+
